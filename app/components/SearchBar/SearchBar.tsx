fb995727ac851b587f890b35f35d5ae55a309a5e
Saman Bzg <samanbzrgvr@gmail.com>
2024-02-21 14:13:14 +0100

    error messages

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import styles from "./SearchBar.module.scss";
 import axios from "axios";
 import SearchResult from "../types/SearchResult";
@@ -8,12 +8,14 @@ import NamedEntity from "../types/NamedEntity";
 import { motion } from "framer-motion";
 
 const SearchBar = ({
+    searchResults,
     setSearchResults,
     setNamedEntities,
     setSummary,
     setLoading,
     loading,
 }: {
+    searchResults: SearchResult[];
     setSearchResults: Dispatch<SetStateAction<SearchResult[]>>;
     setNamedEntities: Dispatch<SetStateAction<NamedEntity[]>>;
     setSummary: Dispatch<SetStateAction<string>>;
@@ -27,47 +29,134 @@ const SearchBar = ({
         namedEntities: 0,
         summary: 0,
     });
+    const [finalStatus, setFinalStatus] = useState("");
 
-    const fetchData = async () => {
-        setLoading(true);
+    useEffect(() => {
+        setFinalStatus(interpretResponseStati());
+    }, [responseStati]);
+
+    const validate = {
+        results: (results: SearchResult[]) => {
+            if (!results || results.length < 2) {
+                setResponseStati((prev) => ({
+                    ...prev,
+                    summary: 404,
+                    searchResults: 404,
+                }));
+                return false;
+            } else {
+                setSearchResults(results);
+                return true;
+            }
+        },
+        summary: (summary: string) => {
+            if (summary && summary.length < 10) {
+                setResponseStati((prev) => ({
+                    ...prev,
+                    summary: 404,
+                }));
+                return false;
+            } else if (summary) {
+                setSummary(summary);
+                return true;
+            }
+        },
+        namedEntities: (namedEntities: NamedEntity[]) => {
+            if (!namedEntities || namedEntities.length < 1) {
+                setResponseStati((prev) => ({
+                    ...prev,
+                    namedEntities: 404,
+                }));
+                return false;
+            } else {
+                setNamedEntities(namedEntities);
+                return true;
+            }
+        },
+    };
+
+    const resetState = () => {
         setSummary("");
         setNamedEntities([]);
         setSearchResults([]);
+        setResponseStati({ searchResults: 0, namedEntities: 0, summary: 0 });
+    };
 
-        const results = await fetchSearchResults();
-        setSearchResults(results);
-        // const summary = await fetchSummary(results);
-        const summary =
-            "this happened that happend and he did this and she did that.";
-        setSummary(summary);
-        const namedEntities = await fetchNamedEntities(results);
+    const fetchData = async () => {
+        resetState();
+        setLoading(true);
+
+        try {
+            const results = await fetchSearchResults();
+            const resultsValid = validate.results(results);
+            if (!resultsValid) {
+                setLoading(false);
+                return;
+            }
+
+            const summary = await fetchSummary(results);
+            validate.summary(summary);
+
+            const namedEntities = await fetchNamedEntities(results);
+            validate.namedEntities(namedEntities);
+        } catch (error) {
+            // setFinalStatus(interpretResponseStati());
+        }
+
         setLoading(false);
-        setNamedEntities(namedEntities);
-
-        console.log(responseStati);
     };
 
     const fetchSummary = async (results: SearchResult[]) => {
         const headlines = results.map((result) => result.article.title);
 
         try {
-            const response = await axios.post(
-                process.env.NEXT_PUBLIC_API_URL + "/summary",
-                {
-                    param: { language: "en" },
-                    headlines: headlines,
-                    topic: searchQuery,
-                }
-            );
+            // const response = await axios.post(
+            //     process.env.NEXT_PUBLIC_API_URL + "/summary",
+            //     {
+            //         param: { language: "en" },
+            //         headlines: headlines,
+            //         topic: searchQuery,
+            //     }
+            // );
 
-            const summary = response.data.summary;
+            // const summary = response.data.summary;
+            const summary =
+                "this happened that happened that happened this and that happened i dont know.";
+            // if (!summary)
+            //     setResponseStati((prev) => ({ ...prev, summary: 404 }));
 
-            if (!summary) return;
             setSearchQuery("");
-            setResponseStati((prev) => ({ ...prev, summary: response.status }));
+            setResponseStati((prev) => ({ ...prev, summary: 200 })); // TODO: 2change later
 
             return summary;
         } catch (error) {
+            setResponseStati((prev) => ({
+                ...prev,
+                summary: error.response.status || 503,
+            }));
+            throw error;
+        }
+    };
+
+    const fetchSearchResults = async () => {
+        try {
+            const response = await axios.get(
+                process.env.NEXT_PUBLIC_API_URL + "/search",
+                {
+                    params: { query: searchQuery, language: "en" },
+                }
+            );
+
+            setResponseStati((prev) => ({
+                ...prev,
+                searchResults: response.status,
+            }));
+            return response.data.results;
+        } catch (error) {
+            setResponseStati((prev) => ({
+                ...prev,
+                searchResults: error.response.status || 503,
+            }));
             throw error;
         }
     };
@@ -86,8 +175,10 @@ const SearchBar = ({
 
             const namedEntities = response.data.named_entities;
 
-            if (!namedEntities) return;
-
+            if (namedEntities.length < 3) {
+                setResponseStati((prev) => ({ ...prev, namedEntities: 404 }));
+                return;
+            }
             setResponseStati((prev) => ({
                 ...prev,
                 namedEntities: response.status,
@@ -97,75 +188,112 @@ const SearchBar = ({
                 ? namedEntities.slice(0, 30)
                 : namedEntities;
         } catch (error) {
-            throw error;
-        }
-    };
-
-    const fetchSearchResults = async () => {
-        try {
-            const response = await axios.get(
-                process.env.NEXT_PUBLIC_API_URL + "/search",
-                {
-                    params: { query: searchQuery, language: "en" },
-                }
-            );
-
             setResponseStati((prev) => ({
                 ...prev,
-                searchResults: response.status,
+                searchResults: error.response.status || 503,
             }));
-
-            return response.data.results;
-        } catch (error) {
             throw error;
         }
     };
 
+    const interpretResponseStati = () => {
+        const {
+            searchResults: searchResultsStatus,
+            namedEntities: namedEntitiesStatus,
+            summary: summaryStatus,
+        } = responseStati;
+
+        class ResponseConditions {
+            static NoResults =
+                searchResultsStatus == 404 || summaryStatus == 404;
+
+            static ServerError = [
+                searchResultsStatus,
+                summaryStatus,
+                namedEntitiesStatus,
+            ].some((status) => status == 503);
+
+            static LimitReached = [
+                searchResultsStatus,
+                summaryStatus,
+                namedEntitiesStatus,
+            ].some((status) => status == 429);
+
+            static NoAnalysis = namedEntitiesStatus == 404;
+
+            static Success = [
+                searchResultsStatus,
+                summaryStatus,
+                namedEntitiesStatus,
+            ].every((status) => status == 200);
+        }
+
+        let errorMessage = "";
+
+        if (ResponseConditions.NoResults) {
+            errorMessage = "No results found for this query, try another one.";
+        } else if (ResponseConditions.ServerError) {
+            errorMessage = "Server is currently down, please try again later.";
+        } else if (ResponseConditions.NoAnalysis) {
+            errorMessage = "No analysis could be made on the search results.";
+        } else if (ResponseConditions.LimitReached) {
+            errorMessage = "Too many searches, try again later.";
+        } else if (ResponseConditions.Success) {
+            errorMessage = "";
+        }
+        return errorMessage;
+    };
     return (
-        <div
-            className={styles.supercontainer}
-            style={
-                loading
-                    ? { opacity: 0.6, cursor: "not-allowed" }
-                    : { opacity: 1, cursor: "pointer" }
-            }
-        >
-            <MeshBackground
-                bgcolor={"#ff8fab"}
-                colors={["#8338ec", "#ff006e", "#a30051"]}
-            />
-            <div className={styles.container}>
+        <div className={styles.container}>
+            <div
+                className={styles.bg_container}
+                style={
+                    loading
+                        ? { opacity: 0.6, cursor: "not-allowed" }
+                        : { opacity: 1, cursor: "pointer" }
+                }
+            >
                 <MeshBackground
-                    bgcolor={"#0d1b2a"}
-                    colors={["#02040f", "#02110d", "#0b132b"]}
+                    bgcolor={"#ff8fab"}
+                    colors={["#8338ec", "#ff006e", "#a30051"]}
                 />
-                <input
-                    disabled={loading}
-                    type="text"
-                    className={styles.search_bar}
-                    placeholder="Search News..."
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button
-                    className={styles.submit_button}
-                    disabled={
-                        loading ||
-                        !searchQuery.match("^[a-zA-Z0-9]*$") ||
-                        searchQuery.length < 3
-                    }
-                    onClick={() => fetchData()}
-                    style={
-                        loading ||
-                        !searchQuery.match(/^[a-zA-Z\s]+[0-9]*$/) ||
-                        searchQuery.length < 3
-                            ? { opacity: 0.6, cursor: "not-allowed" }
-                            : { opacity: 1, cursor: "pointer" }
-                    }
-                >
-                    Search
-                </button>
-            </div>
+
+                <div className={styles.search_bar_container}>
+                    <MeshBackground
+                        bgcolor={"#0d1b2a"}
+                        colors={["#02040f", "#02110d", "#0b132b"]}
+                    />
+                    <input
+                        disabled={loading}
+                        type="text"
+                        className={styles.search_bar}
+                        placeholder="Search News..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <button
+                        className={styles.submit_button}
+                        disabled={
+                            loading ||
+                            !searchQuery.match(/^[a-zA-Z\s]+[0-9]*$/) ||
+                            searchQuery.length < 3
+                        }
+                        onClick={() => fetchData()}
+                        style={
+                            loading ||
+                            !searchQuery.match(/^[a-zA-Z\s]+[0-9]*$/) ||
+                            searchQuery.length < 3
+                                ? { opacity: 0.6, cursor: "not-allowed" }
+                                : { opacity: 1, cursor: "pointer" }
+                        }
+                    >
+                        Search
+                    </button>
+                </div>
+            </div>{" "}
+            {finalStatus && (
+                <p className={styles.error_message}>{finalStatus}</p>
+            )}
         </div>
     );
 };
